f7f574988a58a2abbe2fce784fbe92afccdcbfc2
gsx-git <2559721561@qq.com>
2025-11-24 10:45:12 +0800

    修改

--- vue-shopping/vite.config.js
+++ vue-shopping/vite.config.js
@@ -15,4 +15,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 8088,      // 想改成啥就写啥
+    host: true       // 如需局域网 IP 访问，再加 host: '0.0.0.0'
+  },
 })
